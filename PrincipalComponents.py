8a028a217edaec0c34d22316638f5d18dfd181cc
Joseph Raso <jraso@berkeley.edu>
2021-01-08 20:38:38 -0700

    Fixed PCA variance scaling, redesigned function, updated docs.

--- PrincipalComponents.py
+++ PrincipalComponents.py
@@ -7,27 +7,47 @@ Principal Component Analysis Tools
 
 import numpy as np
 
-def principal_components(X):
-    """Performs the basic function of principal component analysis.
-    Input:
-        X - (ndarray); the data matrix, with rows as samples, and columns as
+class PCA:
+    """Object representing the principal component analysis of input X.
+    Parameters:
+        X - (ndarray) The data matrix, with rows as samples, and columns as
             dimensions. (Is not required to be mean-zero.)
-    Output:
-        s - (ndarray); variance along each principal component, sorted by
-            magnitude.            
-        V - (ndarray); loadings of each principal component as column vectors,
-            sorted by magnitude of the variance (same order as v)"""
-    # Bringing the data to mean-zero
-    Xbar = np.ones(X.shape)*np.average(X,axis=0)
-    B = X - Xbar
-    # Compute the correlation matrix
-    C = np.dot(B.transpose(),B)
-    # Eigenvector analysis
-    u, v = np.linalg.eig(C)
-    # Sorting in order of decending variance
-    a = np.argsort(u)
-    s = np.zeros(u.shape); V = np.zeros(v.shape)
-    for i in range(len(a)):
-        s[-1-i] = u[a[i]]
-        V[-1-i] = v[:,a[i]]
-    return s, V
+    Attributes:
+        X - (ndarray) The original input data.
+        xbar - (ndarray) the mean sample (row) of X.
+        B - (ndarray) The data X, shifted to mean-zero.
+        C - (ndarray) The covariance matrix of B, normalized to the number of
+            samples.
+        s - (ndarray) Variance along each principal component, sorted by
+            magnitude, normalized to the number of samples (rows of X).            
+        V - (ndarray) Loadings of each principal component as column vectors,
+            sorted by magnitude of the variance (same order as v), oriented
+            to put the first dimension of the first principal component in
+            the positive
+            direction.
+        T - (ndarray) The data X, rotated into the PCA basis."""
+    def __init__(self, X):    
+        # Bringing the data to mean-zero
+        xbar = np.average(X,axis=0)
+        B = X - np.ones(X.shape)*xbar
+        # Compute the correlation matrix
+        C = np.dot(B.transpose(),B) / X.shape[0]
+        # Eigenvector analysis
+        u, v = np.linalg.eig(C)
+        # Sorting in order of decending variance
+        a = np.argsort(u)
+        s = np.zeros(u.shape); V = np.zeros(v.shape)
+        for i in range(len(a)):
+            s[-1-i] = u[a[i]]
+            V[-1-i] = v[:,a[i]]
+        V = V*np.sign(V[0,0])
+        # Generating rotated data
+        T = np.dot(X,V)
+        # Storing results:
+        self.X = X
+        self.xbar = xbar
+        self.B = B
+        self.C = C
+        self.s = s
+        self.V = V
+        self.T = T
--- samples/PCA_simple_example.py
+++ samples/PCA_simple_example.py
@@ -15,10 +15,17 @@ import matplotlib.pyplot as plt
 # Creating a sample 2D data set
 N = 500; data = np.zeros((N,2))
 data[:,0] = np.random.normal(size=N)
-data[:,1] = 3*data[:,0] + np.random.normal(size=N)
+data[:,1] = 1*data[:,0] + np.random.normal(size=N)
+
 plt.scatter(data[:,0],data[:,1])
-s, V = principal_components(data)
-# Drawing the principal component at the origin
-plt.quiver(np.zeros((2,1)),np.zeros((2,1)),
-            V[0,:],V[1,:], np.ones((2,1)),
-            angles='xy', scale_units='xy', scale=1)
+
+pca = PCA(data)
+
+# Drawing the principal components at the mean
+plt.quiver(np.ones((2,1))*pca.xbar[0], # x-component of the mean
+           np.ones((2,1))*pca.xbar[1], # y-component of the mean
+           pca.V[0,:]*(pca.s), # x and y components of the pca vectors
+           pca.V[1,:]*(pca.s), # scaled to the variance
+           np.ones((2,1)), angles='xy', scale_units='xy', scale=1)
+            
+#plt.scatter(pca.T[:,0],pca.T[:,1], color='r')
