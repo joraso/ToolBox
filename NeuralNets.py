c5fa967b179744f39d93efe84d83ebbfa3655c6e
Joseph Raso <jraso@berkeley.edu>
2021-02-10 19:13:50 -0700

    Added AutoEncoder to NeuralNets (and example usage to samples).

--- NeuralNets.py
+++ NeuralNets.py
@@ -8,22 +8,24 @@ import numpy as np
 import tensorflow as tf
 
 class ClassifierNet:
-    def __init__(self, inputdims, ncatagories, structure, params={}):
-        """ A neural network design to solve classification problems such as
+    """ A neural network design to solve classification problems such as
         image recognition. The basic construction is an input layer, followed
         by any number of interstitial dense layers (specified by 'structure'),
         and an output layer. The output is a normalized vector of size
         ncatagories, which contains something equivalent to the estimated
         likelyhood that the input sample belongs in that catagory.
-        Parameters:
+        
+        Arguments:
         inputdims   - (tuple) Dimensions of the input data.
         ncatagories - (int) Number of catagories.
         structure   - (list) List of ints specifying the dimensionality (number
                       of neurons) of the interstitial layers.
+                      
+        
         params      - (dict) Other parameters. A dictionary of values to set
                       the attributes (if different from their default values).
         
-        Attributes
+        Attributes:
         inputdims   - (tuple) Dimensions of the input data.
         ncatagories - (int) Number of catagories.
         structure   - (list) List of ints specifying the dimensionality of the
@@ -33,6 +35,8 @@ class ClassifierNet:
         optimizer   - (str) Keras optimizer to use during training. defaults
                      to 'adam'.
         verbose     - (bool) Toggles command line output. Defaults to 1 (T)."""
+    def __init__(self, inputdims, ncatagories, structure, params={}):
+        
         self.inputdims = inputdims
         self.ncatagories = ncatagories
         self.structure = structure
@@ -64,7 +68,7 @@ class ClassifierNet:
                            loss='sparse_categorical_crossentropy')
                            
     def train(self, xtrain, ytrain, batch=1, epochs=1):
-        """Trains the model on the given set.
+        """Trains the model on the given data set.
         Arguments:
         xtrain - (ndarray) Input training data.
         ytrain - (ndarray) Training labels, expected to be an array of ints in
@@ -74,7 +78,7 @@ class ClassifierNet:
         epochs - (int) Number of epochs. defaults to 1.
         """
         self.model.fit(xtrain, ytrain, batch_size=batch, epochs=epochs,
-                       verbose=self.verbose)
+                       verbose=self.verbose, shuffle=True)
         self.loss += (self.model.history.history['loss'])
         self.accuracy += (self.model.history.history['acc'])
         
@@ -90,3 +94,115 @@ class ClassifierNet:
         """
         pred = self.model.predict(xset)
         return np.argmax(pred, axis=1)
+        
+        
+class AutoEncoder():
+    """ The simplest possible auto-encoder neural network design. The basic 
+        construction is:
+        
+            input -> encoder -> latent -> decoder -> output
+        
+        The encoder consists of an input layer, followed by any number of
+        interstitial dense hidden layers (specified by 'structure'), then a low
+        dimensional latent layer. The decoder archetecture is mirror to the
+        encoder. The loss function is the binary cross-entropy between the
+        intou and the reconstructed output.
+        
+        Arguments:
+        inputdims   - (int) Dimensions of the input data. Note that this
+                      network does not perform any data reshaping.
+        latentdims  - (int) Dimension of the latent space.
+        structure   - (list) List of ints specifying the dimensionality (number
+                      of neurons) of the interstitial hidden layers.
+        params      - (dict) Other parameters. A dictionary of values to set
+                      the attributes (if different from their default values).
+        
+        Attributes:
+        inputdims   - (tuple) Dimensions of the input data.
+        latentdims  - (int) Dimension of the latent space.
+        structure   - (list) List of ints specifying the dimensionality of the
+                      interstitial hidden layers.
+        activation  - (str) Keras activation function to be used in the inter-
+                     stitial layers. defaults to 'relu'.
+        optimizer   - (str) Keras optimizer to use during training. defaults
+                     to 'adam'.
+        verbose     - (bool) Toggles command line output. Defaults to 1 (T)."""
+    def __init__(self, inputdims, latentdims, structure, params={}):
+        # Note: input does not perform reshaping
+        self.inputdims = inputdims
+        self.latentdims = latentdims
+        self.structure = structure
+        default_params = {'activation':'relu', 'optimizer':'adam', 'verbose':1}    
+        self.__dict__.update(default_params)
+        self.__dict__.update(params)
+        # History tracking
+        self.loss=[]; self.accuracy=[]
+        # Do the thing:
+        self.Build()
+    def Build(self):
+        """Constructs the model from the specified structure."""
+        def connect(input_layer, layers):
+            # Connects a given list of layers and returns output layer
+            layer = input_layer
+            for l in layers:
+                layer = l(layer)
+            return layer
+        # Define the encoder layers:
+        self.input = tf.keras.Input(shape=self.inputdims) 
+        self.encoding_layers = []
+        for i in self.structure:
+            self.encoding_layers.append(tf.keras.layers.Dense(i,
+                    activation=self.activation))
+        self.latent = tf.keras.layers.Dense(self.latentdims,
+                activation = self.activation)
+        self.encoding_layers.append(self.latent)
+        # Define the decoding layers:
+        self.encoded_input = tf.keras.Input(shape=self.latentdims) 
+        self.decoding_layers = [tf.keras.layers.Dense(self.structure[-1],
+                activation = 'sigmoid')]
+        for i in reversed(self.structure[:-1]):
+            self.decoding_layers.append(tf.keras.layers.Dense(i,
+                    activation=self.activation))
+        self.output = tf.keras.layers.Dense(self.inputdims,                                            
+                activation = self.activation)
+        self.decoding_layers.append(self.output)
+        # Build the intertwinned models
+        self.encoder = tf.keras.Model(self.input,
+                connect(self.input, self.encoding_layers))
+        self.decoder = tf.keras.Model(self.encoded_input,
+                connect(self.encoded_input, self.decoding_layers))
+        self.autoencoder = tf.keras.Model(self.input,
+                connect(self.input, self.encoding_layers+self.decoding_layers))
+        # Compile the auto encoder for training (the intertwined models do
+        # not need compiling, since we don't train them individually)
+        self.autoencoder.compile(optimizer=self.optimizer,
+                metrics=['accuracy'], loss='binary_crossentropy')
+                
+    def train(self, xtrain, batch=1, epochs=1):
+        """Trains the model on the given data set.
+        Arguments:
+        xtrain - (ndarray) Input training data.
+        Keywords:
+        batch  - (int) Batch size. defaults to 1.
+        epochs - (int) Number of epochs. defaults to 1.
+        """
+        self.autoencoder.fit(xtrain, xtrain, batch_size=batch, epochs=epochs,
+                       verbose=self.verbose, shuffle=True)
+        self.loss += (self.autoencoder.history.history['loss'])
+        self.accuracy += (self.autoencoder.history.history['acc'])
+    
+    def Encode(self, xdata):
+        """Returns the latent space/encoded representation of the data.
+        Arguments:
+        xdata - (ndarray) Input data.
+        Returns:
+        xdata - (ndarray) Encoded representation of the data."""
+        return self.encoder.predict(xdata)
+    def Decode(self, zdata):
+        """Returns the reconstruction of output data based on space/encoded
+        representation.
+        Arguments:
+        zdata - (ndarray) Input encoded data.
+        Returns:
+        xdata - (ndarray) Decoded/reconstructed output data."""
+        return self.decoder.predict(zdata)
--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # ToolBox
 
-This project is intended to be a collection of mostly independent functions and tools for data analysis, data science as well as general algorithms and computer science, built for my own education and for potential rapid deployment of tools to projects I may undertake in the future. It also serves as a sort of notebook for me, as I acquire new computational skills, and codify the techniques I already know.
+This project is intended to be a collection of mostly independent functions and tools for data analysis and data science, as well as general algorithms and computer science stuff. This is built for my own education, and for potential rapid deployment of tools to projects I may undertake in the future. It also serves as a sort of notebook for me, as I acquire new computational skills, and codify the techniques I already know.
 
 Currently included is:
 - Principal Component Analysis
 - Classification DNN
 - Binary Tree Structure/Algorithms
+- AutoEncoder DNN
 
 Planned inclusions are:
 - Fast Fourier Transform
-- Autoincoders (possibly variational)
+- variational/convulutional Autoencoders
 - Support vector machines / classic ML methods
 - Multidimensional Linear Regression
 - Monte Carlo optimization
