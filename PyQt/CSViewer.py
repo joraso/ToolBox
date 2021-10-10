3e5bd7e1abcd55911fd67817282ed5cc9d529bbd
Joseph Raso <jraso@berkeley.edu>
2021-10-10 11:44:54 -0700

    Reoganized file structure: PyQt moved out of Python folder and into main.

---
+++ PyQt/CSViewer.py
@@ -0,0 +1,273 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 21 10:02:50 2021
+
+CSViewer - a very simple application for opening and viewing/editing csv files.
+
+This is a work in progress, and currently supports:
+- Opening csv files, and creating empty tables,
+- Ability to saving tables to csv,
+- Editing data and column names,
+- Inserting/deleting new columns/rows.
+
+Features that still need to be added to complete the project:
+- Ability to move/rearrange rows and columns
+
+The new Qt elements experimented with here are:
+- QTables
+- QFileDialog
+- QTabWidget
+- QMainWindow
+- QToolBar
+
+@author: Joe Raso
+"""
+
+import pandas as pd
+from PyQt5 import QtWidgets, QtCore#, QtGui
+
+app = QtWidgets.QApplication([])
+
+# Just the most basic implementation for now.
+# See: https://doc.qt.io/qtforpython-5/PySide2/QtCore/QAbstractTableModel.html
+class csvmodel(QtCore.QAbstractTableModel):
+    def __init__(self, *args, fpath=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        if fpath: self.csv = pd.read_csv(fpath)
+        else: self.csv = pd.DataFrame([0])
+    def data(self, index, role):
+        if role == QtCore.Qt.DisplayRole:
+            # Wierdly, DisplayRole only supports strings
+            return str(self.csv.iloc[index.row(), index.column()])
+    def rowCount(self, index):
+        return len(self.csv)
+    def columnCount(self, index):
+        return len(self.csv.columns)
+        
+    # "Well-behaved" models should also implement:
+    def headerData(self, section, orientation, role=QtCore.Qt.DisplayRole):
+        # Horizontal headers are the columns (section [int] is the index):
+        if orientation == QtCore.Qt.Horizontal and \
+            role == QtCore.Qt.DisplayRole:
+            return self.csv.columns[section]
+        # In every other case, let's return the parent call
+        return super().headerData(section, orientation, role)
+        
+    # For the data to be resizeable, we need to implement the following:
+    def insertRow(self, row, count):
+        """Inserts (count) rows into the table after (row)."""
+        # Mandatory begin statement.
+        self.beginInsertRows(QtCore.QModelIndex(), row, row + count)
+        # On the Pandas end, we're doing this by generating a blank dataframe
+        # and concatenating it into the original
+        insert = pd.DataFrame(index=range(count), columns=self.csv.columns)
+        newcsv = pd.concat([self.csv[:row], insert, self.csv[row:]])
+        newcsv.reset_index()
+        self.csv = newcsv
+        self.endInsertRows() # Mandatory end statement
+    def insertColumn(self, column, count):
+        """Inserts (1) column into the table after (column)."""
+        # Mandatory begin statement. Note the double refence to column and
+        # the lack of refence to count (it's a dummy argument currently).
+        self.beginInsertColumns(QtCore.QModelIndex(), column, column + count-1)
+        # On the Pandas end, we do this by concatentation, as in insertRow
+        insert = pd.Series(index=self.csv.index, dtype=float)
+        newcsv = pd.concat([self.csv[self.csv.columns[:column]], insert,
+                            self.csv[self.csv.columns[column:]]], axis=1)
+        self.csv = newcsv
+        self.endInsertColumns() # Mandatory end statement
+    def removeRows(self, row, count):
+        """Removes (count) rows at (row)."""
+        self.beginRemoveRows(QtCore.QModelIndex(), row, row + count)
+        # Remove rows using 'drop' in Pandas
+        self.csv.drop(labels=range(row,row+count), axis=0, inplace=True)
+        self.csv.reset_index()
+        self.endRemoveRows()
+    def removeColumns(self, column, count):
+        """Removes (1) column at (column)."""
+        self.beginRemoveColumns(QtCore.QModelIndex(), column, column + count-1)
+        # Remove rows using 'drop' in Pandas
+        self.csv.drop(columns=self.csv.columns[column:column + count],
+                      axis=0, inplace=True)
+        self.endRemoveColumns()
+        
+    # These are not standard subclassing functions, but I'm implementing them
+    # here to put values into the underlying DataFrame, and save it as a csv.
+    def editItem(self, index, value):
+        self.csv.iloc[index.row(), index.column()] = value
+        self.layoutChanged.emit() # Emit a layout change signal
+    def editHeader(self, index, value):
+        newname = {self.csv.columns[index.column()]:value}
+        self.csv.rename(columns=newname, inplace=True)
+        self.layoutChanged.emit() # Emit a layout change signal
+    def saveAs(self, fname):
+        self.csv.to_csv(fname)
+    
+class CSViewer(QtWidgets.QMainWindow):
+    def __init__(self, *args, file=None, **kwargs):
+        # This is the proper way of setting up the main window.
+        super().__init__(*args, **kwargs)
+        self.setWindowTitle("CSViewer")
+        self.setGeometry(100,100,500,500)
+        self.setCentralWidget(self.tabSpace())
+        self.addToolBar(self.topBar())
+        # The file kwarg is here for convienience of testing:
+        if file: self.newTab(file)
+        
+    def tabSpace(self):
+        """Generates the central tabbed veiwing space."""
+        self.tabs = QtWidgets.QTabWidget()
+        # Configure the tabs to be closable
+        self.tabs.setTabsClosable(True)
+        # The slot fot tab closure is here:
+        self.tabs.tabCloseRequested.connect(self.closeTab)
+        return self.tabs
+        
+    def inputEntrySpace(self):
+        """Generates and sets up the input field."""        
+        self.field = QtWidgets.QLineEdit()
+        # tie 'enter' on the input field to pushing the value
+        self.field.returnPressed.connect(self.pushValue)
+        # We'll use a string to keep track of where the entered value should
+        # be pushed to (ie. items or header):
+        self.inputFocus = None
+        return self.field
+        
+    def topBar(self):
+        """Generates the tool bar of buttons at the top of the window."""
+        # Trying out QToolBar with actions:
+        self.bar = QtWidgets.QToolBar()
+        # Toolbars typically contain QActions, which can be specified to
+        # addAction in a variety of ways. Note that in the docs, the final
+        # 'Functor' object should be the functional action called.
+        self.openButton = self.bar.addAction("Open", self.openTab)
+        self.newButton = self.bar.addAction("New", self.newTab)
+        self.saveButton = self.bar.addAction("Save", self.saveTab)
+        self.addRowButton = self.bar.addAction("+Row", self.addRow)
+        self.delRowButton = self.bar.addAction("-Row", self.delRow)
+        self.addColButton = self.bar.addAction("+Col", self.addColumn)
+        self.delColButton = self.bar.addAction("-Col", self.delCol)
+        # Toolbars also allow for Widgets like the input entry field
+        self.bar.addWidget(self.inputEntrySpace())
+        # We'll use a simple 
+        return self.bar
+        
+    def newTab(self, fpath=None):
+        """Opens a new tab. If no file path is specified, opens an empty CSV."""
+        model = csvmodel(fpath=fpath)
+        view = QtWidgets.QTableView()
+        view.setModel(model)
+        # Set up what happens when a cell is clicked:      
+        view.clicked.connect(self.displaySelected)
+        # Set up what happens when a column header is clicked:
+        view.horizontalHeader().sectionClicked.connect(self.displayColumnName)
+        # We want to name the tab, so we'll strip the file name from the end
+        # of the file path. If this is a blank tab, then we call it 'untitled'. 
+        fname = "untitled" if not fpath else fpath.split('/')[-1]
+        self.tabs.addTab(view, fname)        
+        
+    def openTab(self):
+        """Select a file, and open it in a new tab."""
+        # This is the simplest way to invoke a file dialog. It's possible to 
+        # set filters and such, but it requires a more complex approach. Note
+        # this function returns a tuple of (abs_path, file_types).
+        fpath_tuple = QtWidgets.QFileDialog.getOpenFileName()
+        # If the user presses cancel, empty strings get returned, and we only
+        # want to open a tab if the user selected a file, so:
+        if fpath_tuple[0]:
+            self.newTab(fpath=fpath_tuple[0])
+        
+    def saveTab(self):
+        """Saves the currentl selected tab."""
+        # As in openTab, but with the SaveFileName instead
+        fpath_tuple = QtWidgets.QFileDialog.getSaveFileName()
+        if fpath_tuple[0]:
+            self.tabs.currentWidget().model().saveAs(fpath_tuple[0])
+        
+    def closeTab(self, currentIndex):
+        """Closes a tab."""
+        # uses the currentIndex arg to identify which tab is being closed.
+        self.tabs.removeTab(currentIndex)
+        
+    def displaySelected(self):
+        """Displays the selected cell on the input field."""
+        # First we have to find the current tab,
+        tab = self.tabs.currentWidget()
+        # then get the index of the selected cell, and the value contained
+        ind = tab.selectionModel().currentIndex()
+        value = tab.model().data(ind, QtCore.Qt.DisplayRole)
+        # and then push it to the display field.
+        self.field.setText(value)
+        
+    def displayColumnName(self):
+        """Displays the column name in the input field."""
+        # Get the tab and the selected header index:
+        tab = self.tabs.currentWidget()
+        ind = tab.horizontalHeader().currentIndex().column()
+        value = str(tab.model().headerData(ind, QtCore.Qt.Horizontal))
+        self.field.setText(value)
+        
+    def pushValue(self):
+        """Deposits the entered value from the input filed into the selected
+        cell, or into the header is a full columnis selected."""
+        # As before, find the tab.
+        tab = self.tabs.currentWidget()
+        # Check if a full column is selected. If so, we are editing the header
+        if tab.selectionModel().selectedColumns():
+            ind = tab.horizontalHeader().currentIndex()
+            # grab the value from the input bar and push it to the csv model.
+            value = self.field.text()
+            tab.model().editHeader(ind, value)
+        else:
+            # Else we are editing a cell
+            ind = tab.selectionModel().currentIndex()
+            # grab the value from the input bar and push it to the csv model.
+            value = self.field.text()
+            tab.model().editItem(ind, value)
+            # Move to the next row down
+            nex = tab.model().index(ind.row()+1, ind.column())
+            tab.selectionModel().setCurrentIndex(nex,
+                QtCore.QItemSelectionModel.ClearAndSelect)
+            self.displaySelected()
+        
+    def addRow(self):
+        """Adds a row at/before the selected row of the selected cell."""
+        # As before, find the tab, and selected cell row.
+        tab = self.tabs.currentWidget()
+        row = tab.selectionModel().currentIndex().row()
+        # Call insert row on the model
+        tab.model().insertRow(row, 1)
+        tab.model().layoutChanged.emit()
+        # Highlight/select the newly inserted row. Note that because it was
+        # inserted above the selected cell, 'row' now refers to the new row.
+        tab.selectRow(row)
+        
+    def addColumn(self):
+        """Adds a column at/before the selected column of the selected cell."""
+        # As in addRow:
+        tab = self.tabs.currentWidget()
+        col = tab.selectionModel().currentIndex().column()
+        tab.model().insertColumn(col, 1)
+        tab.selectColumn(col)
+        tab.model().layoutChanged.emit()
+    
+    def delRow(self):
+        """Deletes the selected row."""
+        tab = self.tabs.currentWidget()
+        row = tab.selectionModel().currentIndex().row()
+        tab.model().removeRows(row, 1)
+        tab.model().layoutChanged.emit()
+        
+    def delCol(self):
+        """Deletes the selected column."""
+        tab = self.tabs.currentWidget()
+        col = tab.selectionModel().currentIndex().column()
+        tab.model().removeColumns(col, 1)
+        tab.model().layoutChanged.emit()
+        
+if __name__ == '__main__':
+    
+    win = CSViewer(file="Aesop.csv")
+    win.show()
+    app.exec()
+#    csv = pd.read_csv("Aesop.csv")
---
+++ PyQt/PopQuiz.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  6 17:47:43 2021
+
+Walking through a rudimentry layout, buttons and signal/slot usage with PyQt5
+by building a True/False popup quiz.
+
+Created with help from:
+https://build-system.fman.io/pyqt5-tutorial
+https://build-system.fman.io/static/public/files/widgets_example.py
+
+@author: Joe Raso
+"""
+
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QHBoxLayout, \
+    QVBoxLayout, QPushButton, QMessageBox
+
+app = QApplication([])
+
+def TrueFalseBox(question):
+    """Little section of code that exicutes a True-False question box."""
+    
+    # Defining the layout layers and window
+    window = QWidget()
+    buttonbox = QWidget()
+    innerlayout = QHBoxLayout()
+    outerlayout = QVBoxLayout()
+
+    # Define the buttons
+    button1 = QPushButton("True")
+    button2 = QPushButton("False")
+    
+    # create a function to exicute when a button is clicked
+    ans = None
+    def click_true():
+        nonlocal ans
+        ans = True
+        window.close()
+    def click_false():
+        nonlocal ans
+        ans = False
+        window.close()
+        
+    # bind the function to the buttons
+    button1.clicked.connect(click_true)
+    button2.clicked.connect(click_false)
+    
+    # Setting the buttons next to each other in the buttonbox
+    innerlayout.addWidget(button1)
+    innerlayout.addWidget(button2)
+    buttonbox.setLayout(innerlayout)
+    
+    # Stacking the buttons with the label in the outer layout
+    label = QLabel(question)
+    outerlayout.addWidget(label)
+    outerlayout.addWidget(buttonbox)
+    window.setLayout(outerlayout)
+    
+    # Exicute the window
+    window.show()
+    app.exec()
+    return ans
+
+def PopQuiz():
+    """Uses the T/F box above to administer a quick little pop quiz."""
+    
+    # Set some questions and their answers in a dictionary
+    questions = {"When water freezes, it's entropy increases.":False,
+        "When water freezes, the entropy of the universe increases":True}
+        
+    # run through the questions and track how many are answered correctly        
+    score = 0
+    for q in questions.keys():
+        ans = TrueFalseBox(q)
+        score += int(ans == questions[q])
+
+    # display the score at the end of the quiz
+    result = QMessageBox()
+    result.setText(f'You scored {score} correct')
+    result.exec()
+    
+PopQuiz()
